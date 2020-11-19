4c01aeb5357d2201658440238b8b53d780d978a0
ibrahimendire <ibrahimendire@users.noreply.github.com>
2020-11-18 19:16:36 -0600

    To Do #1 and #2 done

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -17,6 +17,14 @@ namespace TechJobsMVC.Controllers
             return View();
         }
 
-        // TODO #3: Create an action method to process a search request and render the updated search view. 
+        // TODO #3: Create an action method to process a search request and render the updated search view.
+
+
+
+        public IActionResult Results()
+        {
+           
+            return View();
+        }
     }
 }
